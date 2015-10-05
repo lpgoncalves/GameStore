397dab31282c90bd2f9b219725757f5e0340a47c
lpgoncalves <lp.gooncalves@gmail.com>
2015-10-04 23:01:39 -0300

    Segundo Commit

---
+++ GameStore/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
