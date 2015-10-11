cc1e561b1d07f20f7e889d709b4307143df40401
lpgoncalves <lp.gooncalves@gmail.com>
2015-10-11 00:37:03 -0300

    Realizada a criação de todas as tabelas pelo Lucas.

--- GameStore/src/BancoDados.java
+++ GameStore/src/BancoDados.java
@@ -41,17 +41,41 @@ public class BancoDados {
 				"preco_produto decimal(10,2), " +
 				"PRIMARY KEY (id_produto))"; 
 		
-		String games = "";
+		String games = "CREATE TABLE Games" +
+				"(nome_game varchar(30) NOT NULL, " +
+				"memory_required int, " +
+				"numero_de_pl int, " +
+				"detalhes varchar(50))";
 		
-		String console = "";
+		String console = "CREATE TABLE Consoles" +
+				"(driver_type varchar(30) NOT NULL, " +
+				"size int, " +
+				"detalhes_consoles varchar(50))";		
 		
-		String acessorios = "";
+		String acessorios = "CREATE TABLE Acessorios" + 
+				"(nome_acessorio varchar(20) NOT NULL, " +
+				"descricao_acessorio varchar(50) ," +
+				"detalhes_acessorio varchar(50))";
 		
-		String customer = "";
+		String cliente = "CREATE TABLE Cliente" +
+				"(id_cliente int NOT NULL, " +
+				"codigo_cliente varchar(15) NOT NULL, " +
+				"nome_cliente varchar(20), " +
+				"end_cliente varchar(20), " +
+				"detalhes_cliente varchar(30), " +
+				"PRIMARY KEY (id_cliente))";
 		
-		String customer_order = "";
-		
-		String customer_purchases = "";
+		String pedido_cliente = "CREATE TABLE Pedido_Cliente" +
+				"(id_pedido int NOT NULL, " +
+				"data_pedido date, " +
+				"detalhes_pedido varchar(50), " +
+				"PRIMARY KEY(id_pedido))";
+	
+		String compras_cliente = "CREATE TABLE Compras_Cliente" +
+				"(id_compras int NOT NULL, " +
+				"data_compras date NOT NULL, " +
+				"detalhes_compras varchar(50), " +
+				"PRIMARY KEY(id_compras))";
 		
 		Statement stmt = con.createStatement();
 		
@@ -60,6 +84,24 @@ public class BancoDados {
 		
 		stmt.executeUpdate(tabela_produtos);
 		System.out.println("Tabela Produtos criada com sucesso");
+		
+		stmt.executeUpdate(games);
+		System.out.println("Tabela games criada com sucesso");
+		
+		stmt.executeUpdate(console);
+		System.out.println("Tabela console criada com sucesso");
+		
+		stmt.executeUpdate(acessorios);
+		System.out.println("Tabela acessorios criada com sucesso");
+		
+		stmt.executeUpdate(cliente);
+		System.out.println("Tabela cliente criada com sucesso");
+		
+		stmt.executeUpdate(pedido_cliente);
+		System.out.println("Tabela pedido_cliente criada com sucesso");
+		
+		stmt.executeUpdate(compras_cliente);
+		System.out.println("Tabela compras_cliente criada com sucesso");
 		
 		}catch(Exception e){
 			e.printStackTrace();
@@ -74,7 +116,7 @@ public class BancoDados {
 			
 			stmt.setInt(1, 1);
 			stmt.setString(2, "Teste");
-			stmt.executeUpdate()
+			stmt.executeUpdate();
 */
 
 
