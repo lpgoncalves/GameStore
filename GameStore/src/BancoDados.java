091f9fda8225502491274689f857a62d38bed9ea
Lucas <Lucas@LucasVeronezzi.home>
2015-10-10 21:07:48 -0300

    Adicionado a classe BancoDados em que faz a conexao com o BD e também criei um metodo chamado CriaTabelas em que irá criar todas as tabelas necessárias do Bando de Dados.

---
+++ GameStore/src/BancoDados.java
@@ -0,0 +1,80 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class BancoDados {
+	public BancoDados(){
+		
+	}
+	
+	public Connection ConectaBD(String connectUrl, String user, String senha) {
+		
+		try{
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			Connection con = DriverManager.getConnection(connectUrl, user, senha);
+			System.out.println("Conexão Realizada com sucesso");
+
+			return con;
+			
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+			return null;
+		} catch (Exception e){
+			System.out.println("Não foi possivel conectar" + e);
+			return null;
+		}
+		
+	}
+	
+	public void CriarTabela(Connection con){
+		try{
+		String tabela_tipo_produto = 	"CREATE TABLE Tipo_Produto" +
+				"(id_tipo int NOT NULL, " +
+				"descricao_tipo varchar(100) NOT NULL, " +
+				"PRIMARY KEY (id_tipo)) ";
+		
+		String tabela_produtos = "CREATE TABLE Produtos " +
+				"(id_produto int NOT NULL, " +
+				"descricao_produto varchar(50) NOT NULL, " +
+				"nome_produto varchar(20), " + 
+				"preco_produto decimal(10,2), " +
+				"PRIMARY KEY (id_produto))"; 
+		
+		String games = "";
+		
+		String console = "";
+		
+		String acessorios = "";
+		
+		String customer = "";
+		
+		String customer_order = "";
+		
+		String customer_purchases = "";
+		
+		Statement stmt = con.createStatement();
+		
+		stmt.executeUpdate(tabela_tipo_produto);
+		System.out.println("Tabela Tipo_Produto criada com sucesso");
+		
+		stmt.executeUpdate(tabela_produtos);
+		System.out.println("Tabela Produtos criada com sucesso");
+		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
+
+
+/*
+ * String sql = "INSERT INTO \"TIPO_PRODUTO\" (id_tipo, descricao_tipo) VALUES (?, ?)";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			
+			stmt.setInt(1, 1);
+			stmt.setString(2, "Teste");
+			stmt.executeUpdate();
+*/
+
+
--- GameStore/src/Main.java
+++ GameStore/src/Main.java
@@ -1,12 +1,15 @@
-
 import java.sql.*;
-import java.sql.JDBCType;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
-		conexao.ConectaBD("sa", "123456");
+		String jdbc = "jdbc:sqlserver://localhost:1433;databaseName=GAMESTORE";
+		
+		BancoDados bd = new BancoDados();
+		Connection con = bd.ConectaBD(jdbc, "sa", "123456");
+		
+		bd.CriarTabela(con);
 		
 	}
 
